d98d099af84276a66c90e325af8bf6fec40549c6
sowmik-sec <sowmik.sec@gmail.com>
2023-02-13 16:56:16 +0600

    shopping cart and store in local Storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,14 +1,28 @@
 // use localStorage to manage cart data
 
 const addToDb = (id) => {
-  const quantity = localStorage.getItem(id);
-  if (quantity) {
-    console.log("already exists");
-    const newQuantity = +quantity + 1;
-    localStorage.setItem(id, newQuantity);
+  let shoppingCart = {};
+
+  // get the shopping cart
+  const storedCart = localStorage.getItem("shopping-cart");
+
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
   } else {
-    localStorage.setItem(id, 1);
+    shoppingCart = {};
   }
+
+  // add quantity
+  const quantity = shoppingCart[id];
+  if (quantity) {
+    const newQuantity = +quantity + 1;
+    shoppingCart[id] = newQuantity;
+    // localStorage.setItem(id, newQuantity);
+  } else {
+    shoppingCart[id] = 1;
+    // localStorage.setItem(id, 1);
+  }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 };
 
 export { addToDb };
